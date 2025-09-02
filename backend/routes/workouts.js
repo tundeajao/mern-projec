0e6e1345392baf68b1957fd8b81fb61aaeb6f666
Tunde Ajao <tunde_ajao@hotmail.com>
2025-09-02 20:43:19 +0100

    first commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutContoller')
+
+const router = express.Router()
+
+router.get('/', getWorkouts)
+
+router.get('/:id', getWorkout)
+
+router.post('/', createWorkout)
+
+router.delete('/:id', deleteWorkout)
+
+router.patch('/:id', updateWorkout)
+
+module.exports = router
