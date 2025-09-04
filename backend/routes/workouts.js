8bfac49d1157b46925589871ae639375378ef0fd
Tunde Ajao <tunde_ajao@hotmail.com>
2025-09-04 10:41:50 +0100

    Fourth Commit

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,17 +6,26 @@ const {
     deleteWorkout,
     updateWorkout
 } = require('../controllers/workoutContoller')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
 
+//require auth for all workout routes
+router.use(requireAuth)
+
+//get all workouts
 router.get('/', getWorkouts)
 
+//get a single  workout
 router.get('/:id', getWorkout)
 
+//post a new workout
 router.post('/', createWorkout)
 
+//delete a workout
 router.delete('/:id', deleteWorkout)
 
+//update a workouts
 router.patch('/:id', updateWorkout)
 
 module.exports = router
