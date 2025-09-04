8b7fc98753a7b18c85d532d9313569e4e000722d
Tunde Ajao <tunde_ajao@hotmail.com>
2025-09-04 13:18:23 +0100

    correcting headers

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel')
 const requireAuth = async (req, res, next) => {
 
     //verfy authentication
-    const { authorization } = req.heeaders
+    const { authorization } = req.headers
 
     if (!authorization) {
         return res.status(401).json({error: 'Authorization token required'})
