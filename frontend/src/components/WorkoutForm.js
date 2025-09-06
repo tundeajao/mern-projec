8815fa7c103dab232d7f55f48a25574331ba5942
Tunde Ajao <tunde_ajao@hotmail.com>
2025-09-06 20:34:04 +0100

    Seventh Commit

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const WorkoutForm = () => {
   const { dispatch } = useWorkoutsContext()
+  const { user } = useAuthContext()
 
   const [title, setTitle] = useState('')
   const [load, setLoad] = useState('')
@@ -13,13 +15,19 @@ const WorkoutForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
+    if (!user) {
+      setError('You must be logged in')
+      return
+    }
+
     const workout = {title, load, reps}
 
     const response = await fetch('/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${user.token}'
       }
     })
     const json = await response.json()
@@ -44,28 +52,13 @@ const WorkoutForm = () => {
       <h3>Add a New Workout</h3>
 
       <label>Excersize Title:</label>
-      <input 
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        className={emptyFields.includes('title') ? 'error' : ''}
-      />
+      <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} className={emptyFields.includes('title') ? 'error' : ''} />
 
       <label>Load (in kg):</label>
-      <input 
-        type="number"
-        onChange={(e) => setLoad(e.target.value)}
-        value={load}
-        className={emptyFields.includes('load') ? 'error' : ''}
-      />
+      <input type="number" onChange={(e) => setLoad(e.target.value)} value={load} className={emptyFields.includes('load') ? 'error' : ''} />
 
       <label>Reps:</label>
-      <input 
-        type="number"
-        onChange={(e) => setReps(e.target.value)}
-        value={reps}
-        className={emptyFields.includes('reps') ? 'error' : ''}
-      />
+      <input type="number" onChange={(e) => setReps(e.target.value)} value={reps} className={emptyFields.includes('reps') ? 'error' : ''} />
 
       <button>Add Workout</button>
       {error && <div className="error">{error}</div>}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,23 @@
 import { useEffect } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 //componets
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
 
-
 const Home = () => {
     //const [workouts, setWorkouts] = useState(null)
     const {workouts, dispatch} = useWorkoutsContext();
+    const { user } = useAuthContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'Authorization': 'Bearer ${user.token}'
+                }
+            })
             const json = await response.json()
 
             if (response.ok) {
@@ -21,8 +26,11 @@ const Home = () => {
             }
         }
 
-        fetchWorkouts()
-    }, [dispatch])
+        if (user) {
+            fetchWorkouts()
+        }
+        
+    }, [dispatch, user])
 
     return(
         <div className="home">
